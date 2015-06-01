294acbdf1bad85973c90ddadd59323d590166db9
Torbjörn Nilsson <torgnil@gmail.com>
2015-06-01 22:13:26 +0200

    uci movetime support

--- uci.cpp
+++ uci.cpp
@@ -293,6 +293,9 @@ void uci() {
 			search->hash_size = hash_size;
 			if (line.find("infinite") != string::npos) {
 				search->max_think_time_ms = INT_MAX;
+			} else if (line.find("movetime") != string::npos) {
+				int fixed_move_time_ms = parse_int_parameter(line, "movetime");
+				search->max_think_time_ms = fixed_move_time_ms - 3;
 			} else {
 				int w_time = parse_int_parameter(line, "wtime");
 				int b_time = parse_int_parameter(line, "btime");
