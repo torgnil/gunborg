6823fbe5653f5f7805e0f68acc7bae1220e0419d
Torbjörn Nilsson <torgnil@gmail.com>
2015-06-06 09:17:35 +0200

    version

--- Main.cpp
+++ Main.cpp
@@ -42,7 +42,7 @@ int main(int argc, char* argv[]) {
 	std::cout << setprecision(2);
 	init();
 
-	std::cout << "Gunborg Copyright (C) 2013-2014 Torbjörn Nilsson\n"
+	std::cout << "Gunborg Copyright (C) 2013-2015 Torbjörn Nilsson\n"
 			<< "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n"
 			<< "This is free software, and you are welcome to redistribute it\n"
 			<< "under certain conditions; type `show c' for details.\n";
